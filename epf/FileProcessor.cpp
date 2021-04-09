22a9649c8eed24cb30f6cc0d3948b6c709f99b6f
Piero Toffanin <pt@masseranolabs.com>
2021-04-09 14:24:11 -0400

    Fix compilation on Windows

--- epf/FileProcessor.cpp
+++ epf/FileProcessor.cpp
@@ -50,7 +50,7 @@ void FileProcessor::run()
     // This is some random cell that ultimately won't get used, but it contains a buffer
     // into which we can write data.
     Cell *cell = m_cellMgr.get(VoxelKey());
-    f.setCallback([this, &count, &limit, &cell](pdal::PointRef& point)
+    f.setCallback([this, &count, &limit, &cell, CountIncrement](pdal::PointRef& point)
         {
             // Write the data into the point buffer in the cell.  This is the *last*
             // cell buffer that we used. We're hoping that it's the right one.

05eea6f7b6d57693ad5b1ec8400ddce7ec66402a
Howard Butler <howard@hobu.co>
2021-08-24 14:43:49 -0500

    only need laz-perf to build this

--- ci/linux/setup.sh
+++ ci/linux/setup.sh
@@ -4,8 +4,5 @@ mkdir build
 
 conda update -n base -c defaults conda
 conda install -c conda-forge cmake ninja compilers -y
-
-
-conda install --yes --quiet pdal -y
-conda upgrade laz-perf
+conda install -c conda-forge --yes --quiet laz-perf -y
 
--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -5,6 +5,5 @@ mkdir build
 conda update -n base -c defaults conda
 conda install cmake ninja compilers -y
 
-conda install -c conda-forge pdal -y
-conda upgrade laz-perf -y
+conda install -c conda-forge laz-perf -y
 

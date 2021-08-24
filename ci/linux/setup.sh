c55f975bff49631e6666d373234a08d67543d454
Howard Butler <howard@hobu.co>
2021-08-24 14:49:11 -0500

    need pdal too

--- ci/linux/setup.sh
+++ ci/linux/setup.sh
@@ -4,5 +4,5 @@ mkdir build
 
 conda update -n base -c defaults conda
 conda install -c conda-forge cmake ninja compilers -y
-conda install -c conda-forge --yes --quiet laz-perf -y
+conda install -c conda-forge --yes --quiet laz-perf=2.1.0 pdal -y
 
--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -5,5 +5,5 @@ mkdir build
 conda update -n base -c defaults conda
 conda install cmake ninja compilers -y
 
-conda install -c conda-forge laz-perf -y
+conda install -c conda-forge laz-perf=2.1.0 pdal -y
 

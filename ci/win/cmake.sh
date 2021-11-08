9b0d4dc67d19b854cc9109a746974e9161526c5c
Howard Butler <hobu.inc@gmail.com>
2021-11-08 13:53:08 -0600

    install laz-perf master before builds

--- ci/win/cmake.sh
+++ ci/win/cmake.sh
@@ -4,6 +4,14 @@ pwd
 where cl.exe
 export CC=cl.exe
 export CXX=cl.exe
+
+
+git clone https://github.com/hobu/laz-perf.git && \
+    mkdir build && cd build && \
+    cmake -G Ninja .. -DWITH_TESTS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo  -Dgtest_force_shared_crt=ON -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX && \
+    ninja install && \
+    cd ..
+
 cmake .. -G "Ninja" \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX" \

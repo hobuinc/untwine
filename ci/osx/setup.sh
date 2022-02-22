e6e283dec3a3d394c6d89be47ec8110461e3b061
Andrew Bell <andrew.bell.ia@gmail.com>
2022-02-22 09:10:02 -0500

    Vendor lazperf (#114)
    * Add vendor'd lazperf.
    
    * Link lazperf.
    
    * Remove laz-perf from CI

--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -3,7 +3,8 @@
 mkdir build
 
 conda update -n base -c defaults conda
-conda install cmake ninja compilers -y
+#conda install cmake ninja compilers -y
+conda install cmake ninja -y
 
-conda install -c conda-forge laz-perf pdal -y
+conda install -c conda-forge pdal -y
 

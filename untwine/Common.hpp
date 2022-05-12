63d900a9a6e8f8269a126538946a14c06b7085ac
Andrew Bell <andrew.bell.ia@gmail.com>
2022-05-12 17:07:33 -0400

    Fix more UTF-16 issues.

--- untwine/Common.hpp
+++ untwine/Common.hpp
@@ -88,8 +88,29 @@ inline std::wstring toNative(const std::string& in)
     }
     return out;
 }
+
+inline std::string fromNative(const std::wstring& in)
+{
+    if (in.empty())
+        return std::string();
+
+    int len = WideCharToMultiByte(CP_UTF8, 0, in.data(), in.length(), nullptr, 0, nullptr, nullptr);
+    std::string out(len, 0);
+    if (WideCharToMultiByte(CP_UTF8, 0, in.data(), in.length(), out.data(), len, nullptr, nullptr) == 0)
+    {
+        char buf[200] {};
+        len = FormatMessageA(0, 0, GetLastError(), 0, buf, 199, 0);
+        throw FatalError("Can't convert UTF16 to UTF8: " + std::string(buf, len));
+    }
+    return out;
+}
 #else
 inline std::string toNative(const std::string& in)
+{
+    return in;
+}
+
+inline std::string fromNative(const std::string& in)
 {
     return in;
 }

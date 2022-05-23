3d3bb991e281e9fa14b918a90ee36f7a97f02a80
Howard Butler <hobu.inc@gmail.com>
2022-05-23 11:43:07 -0500

    fix up epf too

--- epf/Epf.cpp
+++ epf/Epf.cpp
@@ -30,6 +30,64 @@
 #include <pdal/util/FileUtils.hpp>
 #include <pdal/util/ProgramArgs.hpp>
 
+
+namespace
+{
+
+// PDAL's directoryList had a bug, so we've imported a working
+// version here so that we can still use older PDAL releases.
+
+#ifndef __APPLE_CC__
+std::vector<std::string> directoryList(const std::string& dir)
+{
+    namespace fs = std::filesystem;
+
+    std::vector<std::string> files;
+
+    try
+    {
+        fs::directory_iterator it(untwine::toNative(dir));
+        fs::directory_iterator end;
+        while (it != end)
+        {
+            files.push_back(untwine::fromNative(it->path()));
+            it++;
+        }
+    }
+    catch (fs::filesystem_error&)
+    {
+        files.clear();
+    }
+    return files;
+}
+#else
+
+#include <dirent.h>
+
+// Provide simple opendir/readdir solution for OSX because directory_iterator is
+// not available until OSX 10.15
+std::vector<std::string> directoryList(const std::string& dir)
+{
+
+    DIR *dpdf;
+    struct dirent *epdf;
+
+    std::vector<std::string> files;
+    dpdf = opendir(dir.c_str());
+    if (dpdf != NULL){
+       while ((epdf = readdir(dpdf))){
+           files.push_back(untwine::fromNative(epdf->d_name));
+       }
+    }
+    closedir(dpdf);
+
+    return files;
+
+}
+#endif
+
+} // unnamed namespace
+
 namespace untwine
 {
 namespace epf
@@ -278,7 +336,7 @@ PointCount Epf::createFileInfo(const StringList& input, StringList dimNames,
     {
         if (FileUtils::isDirectory(filename))
         {
-            std::vector<std::string> dirfiles = FileUtils::directoryList(filename);
+            std::vector<std::string> dirfiles = directoryList(filename);
             filenames.insert(filenames.end(), dirfiles.begin(), dirfiles.end());
         }
         else

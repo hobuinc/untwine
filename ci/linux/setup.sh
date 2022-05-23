79ca2576ae3bab1f88e27f61a18fb32bd91bf9cc
Howard Butler <hobu.inc@gmail.com>
2022-05-23 10:35:43 -0500

    add libstdcxx-ng

--- ci/linux/setup.sh
+++ ci/linux/setup.sh
@@ -4,6 +4,6 @@ mkdir build
 
 conda update -n base -c defaults conda
 #conda install -c conda-forge cmake ninja compilers -y
-conda install -c conda-forge cmake ninja gcc_impl_linux-64 -y
+conda install -c conda-forge cmake ninja libstdcxx-ng>=12.0 -y
 conda install -c conda-forge --yes --quiet pdal -y
 

00a6467c9e623433684765d0561d3301466b9835
Howard Butler <hobu.inc@gmail.com>
2022-06-20 07:47:08 -0500

    start on wmain

--- untwine/Untwine.cpp
+++ untwine/Untwine.cpp
@@ -127,7 +127,11 @@ void createDirs(const Options& options)
 } // namespace untwine
 
 
+#ifdef _WIN32
+int wmain( int argc, wchar_t *argv[ ], wchar_t *envp[ ] )
+#else
 int main(int argc, char *argv[])
+#endif
 {
     std::vector<std::string> arglist;
 

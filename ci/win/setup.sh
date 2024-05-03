25eaaed0789b893af028307ae19f9f8169a592f2
Howard Butler <hobu.inc@gmail.com>
2024-05-03 07:05:56 -0500

    healify CI

--- ci/win/setup.sh
+++ ci/win/setup.sh
@@ -1,9 +1,1 @@
 #!/bin/bash
-
-mkdir build
-
-conda update -n base -c defaults conda
-#conda install -c conda-forge cmake ninja compilers -y
-conda install -c conda-forge cmake ninja -y
-
-conda install -c conda-forge --yes --quiet pdal -y

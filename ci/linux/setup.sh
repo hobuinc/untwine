8f8d4b3d41c3009b79076f9b5d053130bdd4200a
Howard Butler <hobu.inc@gmail.com>
2024-05-03 07:17:54 -0500

    healify CI (#161)

--- ci/linux/setup.sh
+++ ci/linux/setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-mkdir build
 
-conda update -n base -c defaults conda
-conda install -c conda-forge cmake ninja compilers -y
-conda install -c conda-forge --yes --quiet pdal -y
 
--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 
-mkdir build
-
-conda update -n base -c defaults conda
-#conda install cmake ninja compilers -y
-conda install cmake ninja -y
-
-conda install -c conda-forge pdal -y
-

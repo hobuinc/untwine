cba93552a8145bd1f00202777e6e5d01f68a31b0
Howard Butler <hobu.inc@gmail.com>
2024-06-03 11:49:16 -0500

    Use standard BUILD_TESTING CMake variable and deprecate WITH_TESTS
    * streamline CI configuration
    
    * set find_package to CONFIG for PDAL
    
    * use standard BUILD_TESTING cmake config variable and deprecate WITH_TESTS
    
    * BUILD_TESTING now as is standard in CMake instead of WITH_TESTS, which is not
    
    * remove extraneous include

--- ci/linux/cmake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PKG_CONFIG_PATH=$CONDA_PREFIX/lib64/pkgconfig
-
-LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib" cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=Debug \
-      -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
-      ..
--- ci/osx/cmake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cmake -G Ninja \
-      -DCMAKE_BUILD_TYPE=Debug \
-      -DCMAKE_FIND_FRAMEWORK="NEVER" \
-      -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
-      ..

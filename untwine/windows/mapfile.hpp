f96fdb6fa0e4647d33a8524cb868cba6df3795bd
Andrew Bell <andrew.bell.ia@gmail.com>
2024-06-28 13:14:39 -0400

    Inline os functions.

--- untwine/windows/mapfile.hpp
+++ untwine/windows/mapfile.hpp
@@ -34,7 +34,7 @@ public:
     HANDLE m_handle;
 };
 
-MapContext mapFile(const std::string& filename, bool readOnly, size_t pos, size_t size)
+inline MapContext mapFile(const std::string& filename, bool readOnly, size_t pos, size_t size)
 {
     MapContext ctx;
 
@@ -65,7 +65,7 @@ MapContext mapFile(const std::string& filename, bool readOnly, size_t pos, size_
     return ctx;
 }
 
-MapContext unmapFile(MapContext ctx)
+inline MapContext unmapFile(MapContext ctx)
 {
     if (UnmapViewOfFile(ctx.m_addr) == 0)
         ctx.m_error = "Couldn't unmap file.";
